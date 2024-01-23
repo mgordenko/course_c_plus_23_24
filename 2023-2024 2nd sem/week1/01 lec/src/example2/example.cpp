53e2a34057495551c527882bbeea2c0725e81d16
Mariia K <mkgordenko@gmail.com>
2024-01-24 00:34:33 +0300

    lec3

--- 2023-2024 2nd sem/week1/01 lec/src/example2/example.cpp
+++
@@ -1,5 +0,0 @@
-#include "example.h"
-
-int greet() {
-    return 123;
-}
--- 2023-2024 2nd sem/week1/01 lec/src/example2/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "example.cpp"
-
-using namespace std;
-
-int main() {
-    cout << greet()  << endl;
-    return 0;
-}
