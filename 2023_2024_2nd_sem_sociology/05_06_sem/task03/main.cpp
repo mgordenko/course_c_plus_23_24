2d2a2bb4b895b8378f5eb10ff3a4e0e05b81a864
Mariia K <mkgordenko@gmail.com>
2024-01-27 09:50:59 +0300

    5-6 sem soc

--- 2023_2024_2nd_sem_sociology/05_06_sem/task03/main.cpp
+++ 2023_2024_2nd_sem_sociology/05_06_sem/task03/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 
+#include <iostream>
+
 int main() {
   int n = 5;
   int* arr = new int[n];
-  for (int i = 0; i < n; i++) {
-    arr[i] = i;
-  }
+
   for (int i = 0; i < n; i++) {
     std::cout << arr[i] << " ";
   }
@@ -14,5 +14,5 @@ int main() {
     std::cout << *(arr + i) << " ";
   }
   std::cout << "\n";
-  delete[] arr;
+
 }
