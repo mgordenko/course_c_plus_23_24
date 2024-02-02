299f7d9b73864e564e1cb38f8455afb8653d5f40
pelancha <sobachekrokky@gmail.com>
2024-02-02 10:09:41 +0300

    check

---
+++ build/CMakeFiles/problem1_circleArea.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nadezhdafomicheva/Documents/course_c_plus_23_24-1/2023_2024_2nd_sem/week4/07_08_sem/problem1_circleArea/main.cpp" "CMakeFiles/problem1_circleArea.dir/problem1_circleArea/main.cpp.o" "gcc" "CMakeFiles/problem1_circleArea.dir/problem1_circleArea/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/problem1_circleArea.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/problem1_circleArea.dir/problem1_circleArea/main.cpp.o"
+  "CMakeFiles/problem1_circleArea.dir/problem1_circleArea/main.cpp.o.d"
+  "problem1_circleArea"
+  "problem1_circleArea.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/problem1_circleArea.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
